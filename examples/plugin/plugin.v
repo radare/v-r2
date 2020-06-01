6baf48a578ce8bc9278ab6b5cb4b0df773b85ed6
pancake <pancake@nopcode.org>
2020-06-01 18:06:51 +0200

    Remove inline C. code

--- examples/plugin/plugin.v
+++ examples/plugin/plugin.v
@@ -21,15 +21,9 @@ pub struct RCore {}
 fn plugin_call(coreptr voidptr, cmd byteptr) int {
 	core := r2.cast(coreptr)
 	c := string(cmd)
-	// eprintln('calling v code to handle this command ($c)')
 	if c.starts_with('v') {
-		res := core.cmd('?e hello')
-		eprintln('res = $res')
-		eprintln('res = $res')
-		// eprintln('Intercepted!')
-		C.r_core_cmd (core, '?E CLIPPY', 0)
-		ij := string(C.r_core_cmd_str (core, 'ij'))
-		a := json.decode(R2ij, ij) or {
+		core.cmd('?E Clippy')
+		a := json.decode(R2ij, core.cmd('ij')) or {
 			eprintln ('failed to decode ij JSON')
 			return 0
 		}
@@ -41,7 +35,6 @@ fn plugin_call(coreptr voidptr, cmd byteptr) int {
 
 // definition
 
-// #include "@VROOT/examples/plugin/plugin.v.c"
 pub const (
 	dummy_core_plugin = C.r_core_plugin_t {
 		name: 'vr'.str
@@ -49,7 +42,6 @@ pub const (
 		license: 'LGPL3'.str
 		call: plugin_call
 	}
-	// TODO: _const_vr2__radare_plugin
 	radare_plugin = C.r_lib_struct_t {
 		@type: C.R_LIB_TYPE_CORE
 		data: &dummy_core_plugin
