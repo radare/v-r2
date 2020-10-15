6aa64263d8907b2acb6135f695235e3d7e78531b
pancake <pancake@nopcode.org>
2020-10-15 04:40:23 +0200

    Fix build for latest V and do not use pkg-config

--- r2_plugin.v
+++ r2_plugin.v
@@ -2,10 +2,12 @@ module r2
 
 #include <r_core.h>
 #include <r_lib.h>
-#flag `pkg-config --cflags --libs r_core`
+// #flag `pkg-config --cflags --libs r_core`
+#flag -I/usr/local/include/libr -I/usr/local/include/libr/sdb -L/usr/local/lib -lr_core -lr_config -lr_debug -lr_bin -lr_anal -lr_bp -lr_egg -lr_asm -lr_lang -lr_parse -lr_flag -lr_reg -lr_search -lr_syscall -lr_fs -lr_io -lr_socket -lr_cons -lr_magic -lr_crypto -lr_hash -lr_util -ldl
+#flag -I/usr/include/libr -I/usr/include/libr/sdb -L/usr/lib -lr_core -lr_config -lr_debug -lr_bin -lr_anal -lr_bp -lr_egg -lr_asm -lr_lang -lr_parse -lr_flag -lr_reg -lr_search -lr_syscall -lr_fs -lr_io -lr_socket -lr_cons -lr_magic -lr_crypto -lr_hash -lr_util -ldl
 
-type CorePluginInitCallback fn(voidptr,byteptr) int
-type CorePluginCallCallback fn(voidptr,byteptr) int
+type CorePluginInitCallback = fn(voidptr,byteptr) int
+type CorePluginCallCallback = fn(voidptr,byteptr) int
 
 pub struct C.r_lib_struct_t {
 	@type int
